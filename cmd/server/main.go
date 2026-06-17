5a90fd3560dd95c8b0bf5b03332ecc02b1f216ff
DarrenMannuela <darren.mannuela@gmail.com>
2026-06-17 16:45:25 +0700

    changed id parameter to become wild card pattern to allow urlencoded id to pas to the backend

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -41,57 +41,57 @@ func main() {
 		// Order Entry
 		v1.GET("/order", handler.GetOrders)
 		v1.POST("/order", handler.PostOrders)
-		v1.PATCH("/order/:id", handler.UpdateOrders)
-		v1.DELETE("/order/:id", handler.DeleteOrders)
+		v1.PATCH("/order/*id", handler.UpdateOrders)
+		v1.DELETE("/order/*id", handler.DeleteOrders)
 
 		// Delivery Entry
 		v1.GET("/delivery", handler.GetDelivery)
 		v1.POST("/delivery", handler.PostDelivery)
-		v1.PATCH("/delivery/:id", handler.UpdateDelivery)
-		v1.DELETE("/delivery/:id", handler.DeleteDelivery)
+		v1.PATCH("/delivery/*id", handler.UpdateDelivery)
+		v1.DELETE("/delivery/*id", handler.DeleteDelivery)
 
 		// Supplier Entry
 		v1.GET("/supplier", handler.GetSupplier)
 		v1.POST("/supplier", handler.PostSupplier)
-		v1.GET("/supplier/:id", handler.GetSupplierByID)
-		v1.PATCH("/supplier/:id", handler.UpdateSupplier)
-		v1.DELETE("/supplier/:id", handler.DeleteSupplier)
+		v1.GET("/supplier/*id", handler.GetSupplierByID)
+		v1.PATCH("/supplier/*id", handler.UpdateSupplier)
+		v1.DELETE("/supplier/*id", handler.DeleteSupplier)
 
 		// Production Entry
 		v1.GET("/production", handler.GetProduction)
 		v1.POST("/production", handler.PostProduction)
-		v1.PATCH("/production/:id", handler.UpdateProduction)
+		v1.PATCH("/production/*id", handler.UpdateProduction)
 		v1.DELETE("/production/*id", handler.DeleteProduction)
 
 		// Operation Entry
 		v1.GET("/operation", handler.GetOperation)
 		v1.POST("/operation", handler.PostOperation)
-		v1.PATCH("/operation/:id", handler.UpdateOperation)
-		v1.DELETE("/operation/:id", handler.DeleteOperation)
+		v1.PATCH("/operation/*id", handler.UpdateOperation)
+		v1.DELETE("/operation/*id", handler.DeleteOperation)
 
 		// Order-Recap Entry
 		v1.GET("/order-recap", handler.GetOrderRecap)
 		v1.POST("/order-recap", handler.PostOrderRecap)
-		v1.PATCH("/order-recap/:id", handler.UpdateOrderRecap)
-		v1.DELETE("/order-recap/:id", handler.DeleteOrderRecap)
+		v1.PATCH("/order-recap/*id", handler.UpdateOrderRecap)
+		v1.DELETE("/order-recap/*id", handler.DeleteOrderRecap)
 
 		// Surat Jalan Entry
 		v1.GET("/surat-jalan", handler.GetSuratJalan)
 		v1.POST("/surat-jalan", handler.PostSuratJalan)
-		v1.PATCH("/surat-jalan/:id", handler.UpdateSuratJalan)
-		v1.DELETE("/surat-jalan/:id", handler.DeleteSuratJalan)
+		v1.PATCH("/surat-jalan/*id", handler.UpdateSuratJalan)
+		v1.DELETE("/surat-jalan/*id", handler.DeleteSuratJalan)
 
 		// Item Entry
 		v1.GET("/item", handler.GetItems)
 		v1.POST("/item", handler.PostItems)
-		v1.PATCH("/item/:id", handler.UpdateItems)
-		v1.DELETE("/item/:id", handler.DeleteItems)
+		v1.PATCH("/item/*id", handler.UpdateItems)
+		v1.DELETE("/item/*id", handler.DeleteItems)
 
 		// Delivery Order Entry
 		v1.GET("/delivery-order", handler.GetDeliveryOrder)
 		v1.POST("/delivery-order", handler.PostDeliveryOrder)
-		v1.PATCH("/delivery-order/:id", handler.UpdateDeliveryOrder)
-		v1.DELETE("/delivery-order/:id", handler.DeleteDeliveryOrder)
+		v1.PATCH("/delivery-order/*id", handler.UpdateDeliveryOrder)
+		v1.DELETE("/delivery-order/*id", handler.DeleteDeliveryOrder)
 	}
 
 	// Start server on port 8000 to match your OpenAPI 'servers' list
