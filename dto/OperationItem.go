f8e8bb8d0c4e071779607b11fb44dc38fee1c3d8
DarrenMannuela <darren.mannuela@gmail.com>
2026-07-25 15:30:15 +0700

    update on operation item

--- dto/OperationItem.go
+++ dto/OperationItem.go
@@ -3,6 +3,7 @@ package dto
 type OperationItem struct {
 	Id          uint          `json:"id" gorm:"primaryKey;autoIncrement"`
 	HeaderId    string        `json:"header_id"`
+	Category    string        `json:"category"`
 	Description string        `json:"description"`
 	Price       int64         `json:"price"`
 	Header      FinanceHeader `json:"-" gorm:"foreignKey:HeaderId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
